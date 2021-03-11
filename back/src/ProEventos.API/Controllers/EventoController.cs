ef6174514ae54ad1f1ed3c7be5642d0360450167
Pedro Sanches <pedrohensanches@outlook.com>
2021-03-11 00:14:50 -0300

    Até a aula 177 - Postman
    Alterada controller para EventoController, retirado questões de logger e adicionado alguns exemplos de http para executar no postman

---
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "exemplo get";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "exemplo post";
+        }
+        
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"exemplo put com id = {id}";
+        }
+        
+        [HttpDelete("{id}")]
+        public string Deleter(int id)
+        {
+            return $"exemplo delete com id = {id}";
+        }
+    }
+}
