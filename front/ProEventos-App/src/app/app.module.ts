765ea4b2b5b302aa012700e70e98a0f1aa3f52d6
Pedro Sanches <pedrohensanches@outlook.com>
2021-03-25 00:57:13 -0300

    Até a aula 203 - Navbar
    Adicionado navbar usando o bootstrap

--- front/ProEventos-App/src/app/app.module.ts
+++ front/ProEventos-App/src/app/app.module.ts
@@ -7,14 +7,23 @@ import { AppComponent } from './app.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
-  declarations: [AppComponent, PalestrantesComponent, EventosComponent],
+  declarations: [
+    AppComponent,
+    PalestrantesComponent,
+    EventosComponent,
+    NavComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    CollapseModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
