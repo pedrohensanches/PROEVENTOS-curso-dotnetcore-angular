4be410fa441e649a2b28242b31fbfa760c55cded
Pedro Sanches <pedrohensanches@outlook.com>
2021-03-27 15:20:52 -0300

    Até a aula 212 - Filtrando Grid
    Trabalhando com exibiçã ode imagem com curso usando o collapse animado e filtro da grid utilizando o ngModel + FormsModule

--- front/ProEventos-App/src/app/app.module.ts
+++ front/ProEventos-App/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { EventosComponent } from './eventos/eventos.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-
-import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  larguraImg = 150;
+  margenImg = 2;
+  exibeImg = false;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(v: string) {
+    this._filtroLista = v;
+    this.eventosFiltrados =
+      this.filtroLista && this.filtroLista !== ''
+        ? this.filtrarEventos(this.filtroLista)
+        : this.eventos;
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -15,10 +32,23 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public filtrarEventos(query: string): any {
+    query = query.toLocaleLowerCase();
+    return this.eventos.filter(
+      (e: any) =>
+        e.tema.toLocaleLowerCase().indexOf(query) !== -1 ||
+        e.local.toLocaleLowerCase().indexOf(query) !== -1
+    );
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      (response) => (this.eventos = response),
+      (response) => (this.eventos = this.eventosFiltrados = response),
       (error) => console.log(error)
     );
+  }
+
+  public exibirImg(): void {
+    this.exibeImg = !this.exibeImg;
   }
 }
