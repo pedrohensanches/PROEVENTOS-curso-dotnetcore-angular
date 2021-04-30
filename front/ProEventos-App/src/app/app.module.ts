9f34954ac2a4d74fc7821ed184be52f0900b3eae
Pedro Sanches <pedrohensanches@outlook.com>
2021-04-29 21:50:12 -0300

    Até a aula 252 - NGX Spinner - Carregando
    Adicionado modal para confirmação do botão excluir evento
    
    Adicionado aviso toastr (efeito torradeira onde o aviso surge)
    
    Adicionado carregando spinner (efeito aguarde)

--- front/ProEventos-App/src/app/app.module.ts
+++ front/ProEventos-App/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,6 +7,9 @@ import { FormsModule } from '@angular/forms';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppComponent } from './app.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
@@ -34,8 +37,17 @@ import { EventoService } from './services/evento.service';
     CollapseModule.forRoot(),
     TooltipModule.forRoot(),
     BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+    }),
+    NgxSpinnerModule
   ],
   providers: [EventoService],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { Evento } from '../models/Evento';
 import { EventoService } from '../services/evento.service';
 
@@ -9,6 +12,7 @@ import { EventoService } from '../services/evento.service';
   // providers: [EventoService]
 })
 export class EventosComponent implements OnInit {
+  public modalRef = {} as BsModalRef;
   public eventos: Evento[] = [];
   public eventosFiltrados: Evento[] = [];
   public larguraImg = 150;
@@ -28,10 +32,16 @@ export class EventosComponent implements OnInit {
         : this.eventos;
   }
 
-  constructor(private eventoService: EventoService) {}
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private toastrService: ToastrService,
+    private spinner: NgxSpinnerService
+  ) {}
 
   public ngOnInit(): void {
     this.getEventos();
+    this.spinner.show();
   }
 
   public filtrarEventos(query: string): Evento[] {
@@ -49,11 +59,34 @@ export class EventosComponent implements OnInit {
       next: (eventos: Evento[]) => {
         this.eventos = this.eventosFiltrados = eventos;
       },
-      error: (error: any) => console.log(error),
+      error: (error: any) => {
+        this.spinner.hide();
+        this.toastrService.error(
+          'Erro ao carregar eventos!',
+          'Erro!'
+        );
+      },
+      complete: () => this.spinner.hide(),
     });
   }
 
   public exibirImg(): void {
     this.exibeImg = !this.exibeImg;
+  }
+
+  public openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+  }
+
+  public confirm(): void {
+    this.modalRef.hide();
+    this.toastrService.success(
+      'O evento foi deletado com sucesso.',
+      'Deletado!'
+    );
+  }
+
+  public decline(): void {
+    this.modalRef.hide();
   }
 }
